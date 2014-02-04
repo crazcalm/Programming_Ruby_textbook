8569ef63436e2b78cb91ab5667e8d2b0640a0558
Marcus Willock <crazcalm@gmail.com>
2014-02-03 19:38:26 -0500

    variables are references to objects

--- Chapter_3/accounting_example.rb
+++ Chapter_3/accounting_example.rb
@@ -47,3 +47,38 @@ puts "After $50 transaction:"
 puts "Savings balance: #{savings.balance}"
 puts "Checking balance: #{checking.balance}"
 
+
+
+
+# person example
+person = "Tim"
+puts "\nThe object in 'person' is a #{person.class}"
+puts "the object has an id of #{person.object_id}"
+puts "and a value of '#{person}'"
+
+person1 = "Tim"
+person2 = person1
+person1[0] = 'J' # shows that strings are mutable!
+
+puts "\nperson1 is #{person1}"
+puts "person2 is #{person2}"
+
+puts "\nperson1 id: #{person1.object_id}" # same id means that they reference the same object.
+puts "person2 id: #{person2.object_id}"
+
+=begin
+Notes:
+
+1. There are two different ways to deal with encapsulation
+
+2. When appending to the end of an array, push can add many items and << can only add one item.
+
+3. in the constructor, attr_reader :item will give you access to the item,
+	 and protected :item, will make the item protected. Can use both at the same time.
+
+4. Everything is an object (never forget it!)
+
+5. Correction: variables are not objects. They are references to objects. Check out person example.
+
+6. Strings are mutable!
+=end
